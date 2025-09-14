63a1d30ce485ae0327fc887b4985f105733d9621
DaavidK04 <daviidprivat@gmail.com>
2025-09-14 18:27:08 +0200

    sprites für unterschiedliche richtungen (brauch ich evtl gar nicht weil man in pygame rotieren kann... und halt basic movement mit pfeiltasten klappt jz

--- test.py
+++ test.py
@@ -18,12 +18,23 @@ tyre_rect = tyre_srfc.get_rect(center=(600, 300))
 tyre_rect.inflate_ip(-16 , -20)
 
 
-racecar_srfc = pygame.image.load("res/racecar_final.png").convert_alpha()
-racecar_srfc = pygame.transform.scale(racecar_srfc, (64, 64))
-racecar_rect = racecar_srfc.get_rect(center=(400, 300))
-racecar_rect.inflate_ip(-36, -12)
+racecar_up_srfc = pygame.image.load("res/racecar_final.png").convert_alpha()
+racecar_right_srfc = pygame.image.load("res/racecar_right.png").convert_alpha()
+racecar_down_srfc = pygame.image.load("res/racecar_down.png").convert_alpha()
+racecar_left_srfc = pygame.image.load("res/racecar_left.png").convert_alpha()
 
+car_sprites = {
+    "up": (racecar_up_srfc, (-36, -12)),     
+    "right": (racecar_right_srfc, (-12, -36)), 
+    "down": (racecar_down_srfc, (-36, -12)),
+    "left": (racecar_left_srfc, (-12, -36)),
+    "schraeg": (pygame.transform.rotate(racecar_right_srfc, -20), (-12, -36))
+}
 
+direction = "right"
+current_srfc, hitbox_adjust = car_sprites[direction]
+racecar_rect = current_srfc.get_rect(center=(400, 300))
+racecar_rect.inflate_ip(*hitbox_adjust)
 
 while True:
     for event in pygame.event.get():
@@ -31,39 +42,37 @@ while True:
             pygame.quit()
             exit()
 
-    
-    
-    screen.blit(background_srfc, (0,0))
-
-    screen.blit(tyre_srfc, tyre_rect)
-    pygame.draw.rect(screen, "red", racecar_rect, 1)
-
-    pygame.draw.rect(screen, "blue", tyre_rect, 1)
-    screen.blit(racecar_srfc, racecar_rect)
-    # if racecar_rect.colliderect(tyre_rect):
-    #     racecar_rect.x = 0
-    # else:
-    #     racecar_rect.x += 0.5
-        
-    screen.blit(text_srfc, (0, 0))
-    
-    #print(racecar_rect.colliderect(tyre_rect))
-    # mouse_pos = pygame.mouse.get_pos()
-    # if racecar_rect.collidepoint((mouse_pos)):
-    #     print(pygame.mouse.get_pressed())
-    # else:
-    #     print("no collision")
-    
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_RIGHT]:
+        direction = "right"
         racecar_rect.x += 3
     if keys[pygame.K_LEFT]:
+        direction = "left"
         racecar_rect.x -= 3
     if keys[pygame.K_UP]:
+        direction = "up"
         racecar_rect.y -= 3
     if keys[pygame.K_DOWN]:
+        direction = "schraeg"
         racecar_rect.y += 3
+
+    current_srfc, hitbox_adjust = car_sprites[direction]
+
+    center = racecar_rect.center
+    racecar_rect = current_srfc.get_rect(center=center)
+    racecar_rect.inflate_ip(*hitbox_adjust)
+
+    screen.blit(background_srfc, (0,0))
+    screen.blit(tyre_srfc, tyre_rect)
+
+    pygame.draw.rect(screen, "blue", tyre_rect, 1)
+    pygame.draw.rect(screen, "red", racecar_rect, 1)
+
+    screen.blit(current_srfc, racecar_rect)
+    screen.blit(text_srfc, (0, 0))
+
+    print(racecar_rect.colliderect(tyre_rect))
 
 
     pygame.display.update()
